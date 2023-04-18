e8bce01f02c7487689fafcfabe2f8d54b8c58916
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-04-18 11:48:30 -0700

    added magic 8 ball

---
+++ src/EightBall.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import './EightBall.css';
+
+const defaultAnswers = [
+	{ msg: 'It is certain.', color: 'green' },
+	{ msg: 'It is decidedly so.', color: 'green' },
+	{ msg: 'Without a doubt.', color: 'green' },
+	{ msg: 'Yes - definitely.', color: 'green' },
+	{ msg: 'You may rely on it.', color: 'green' },
+	{ msg: 'As I see it, yes.', color: 'green' },
+	{ msg: 'Most likely.', color: 'green' },
+	{ msg: 'Outlook good.', color: 'green' },
+	{ msg: 'Yes.', color: 'green' },
+	{ msg: 'Signs point to yes.', color: 'goldenrod' },
+	{ msg: 'Reply hazy, try again.', color: 'goldenrod' },
+	{ msg: 'Ask again later.', color: 'goldenrod' },
+	{ msg: 'Better not tell you now.', color: 'goldenrod' },
+	{ msg: 'Cannot predict now.', color: 'goldenrod' },
+	{ msg: 'Concentrate and ask again.', color: 'goldenrod' },
+	{ msg: "Don't count on it.", color: 'red' },
+	{ msg: 'My reply is no.', color: 'red' },
+	{ msg: 'My sources say no.', color: 'red' },
+	{ msg: 'Outlook not so good.', color: 'red' },
+	{ msg: 'Very doubtful.', color: 'red' },
+];
+
+function EightBall({ answers = defaultAnswers }) {
+	const [answer, setAnswer] = useState('Think of a Question');
+	const [color, setColor] = useState('black');
+
+	function getRandomIndex(arr) {
+		return Math.floor(Math.random() * arr.length);
+	}
+
+	function getNewAnswer() {
+		const index = getRandomIndex(answers);
+		const { msg, color } = answers[index];
+
+		setAnswer(msg);
+		setColor(color);
+	}
+
+	return (
+		<div
+			className="EightBall"
+			style={{ 'background-color': color }}
+			onClick={getNewAnswer}>
+			<p>{answer}</p>
+		</div>
+	);
+}
+
+export default EightBall;
